f7fdc211d155410db12e60674586de6910793dba
Zsolt <zsoltifju@gmail.com>
2024-03-24 23:13:09 +0100

    task 01 task 02 task 03 task 04 task 05

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,14 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+  let sum = 0;
+  for (const vizsgalt of input) {
+    if (typeof vizsgalt === 'number' && !isNaN(vizsgalt)) {
+      sum += vizsgalt;
+    } else if (Array.isArray(vizsgalt)) {
+        if(!isNaN(arraySum(vizsgalt)))
+          sum += arraySum(vizsgalt);
+    }
+  }
+  return sum;
 };
--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,33 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  /*let sum = 0;
+  for(const i of games) {
+    let red = 0, green = 0, blue = 0;
+    for(const j of i.draws) {
+        red = Math.max(red, j.red??0);
+        green = Math.max(green, j.green??0);
+        blue = Math.max(blue, j.blue??0);
+    }
+    sum += (red*green*blue); 
+  }
+  return sum;
+};
+Átírva reduce függvénybe: 
+*/
+  const sum = games.reduce((totalSum, currentGame) => {
+    const maxValues = currentGame.draws.reduce((max, draw) => {
+      const red = draw.red ?? 0;
+      const green = draw.green ?? 0;
+      const blue = draw.blue ?? 0;
+      return {
+        red: Math.max(max.red ??0, red),
+        green: Math.max(max.green??0, green),
+        blue: Math.max(max.blue??0, blue)
+      };
+    }, { red: 0, green: 0, blue: 0 });
+    const gameSum = (maxValues.red ?? 0) * (maxValues.green ?? 0) * (maxValues.blue ?? 0);
+    return totalSum + gameSum;
+  }, 0);
+  return sum;
 };
